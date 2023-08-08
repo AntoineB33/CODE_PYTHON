74e9724fe1816539c88e79c59b806e63d47234f1
AntoineB33 <antoine.barbault@gmail.com>
2023-08-08 11:46:40 +0200

    removing errors

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-ar = np.array([[1, 2, 3],[8,5,6]])
-sub = np.array([1, 0])
-ar[sub] = 2
-print(ar)
+# Create a sample 3D array
+array_3d = np.array([[[0,0,0],
+                      [0,1, 6]],
+                     
+                     [[7, 8, 9],
+                      [10, 11, 12]]])
+
+# Sum along a specified axis (e.g., axis=2)
+sum_result = np.sum(array_3d, axis=(1, 2))
+
+print(sum_result)
