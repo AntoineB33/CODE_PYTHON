7d0b5e8ddb29b0a7466046249573f9a9c9d00d99
AntoineB33 <antoine.barbault@gmail.com>
2023-08-21 14:56:49 +0200

    in progress

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-import numpy as np
-
-# Create a sample 3D matrix
-matrix_3d = np.array([
-    [[1, 2, 3], [4, 5, 6]],
-    [[7, 8, 9], [10, 11, 12]]
-])
-
-# Sum along the second axis (axis=1) to get a 1D array
-sum_array = np.sum(matrix_3d, axis=(0,2))
-
-print(sum_array)
+print(1/2)
